f4fcb1b32801d7a55c0051fb1d536b73f271eecb
Chielo Newctle <ChieloNewctle@gmail.com>
2025-08-01 10:33:57 +0800

    misc(format): format rust code

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -3,7 +3,7 @@ mod prefix_dfs;
 use ::mtc_token_healing::{SortedTokenRange, TokenId, VocabPrefixAutomaton};
 use pyo3::prelude::*;
 
-use crate::prefix_dfs::{dfs_token_seq_trie_py, TokenSeqTrieNode};
+use crate::prefix_dfs::{TokenSeqTrieNode, dfs_token_seq_trie_py};
 
 #[pymodule]
 fn mtc_token_healing(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
